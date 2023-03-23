9f7ff73a72dd1487ffe1711974c8e7c6191d5190
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2023-03-23 09:46:50 +0100

    Add meta tags

--- resources/views/layout/app-layout.blade.php
+++ resources/views/layout/app-layout.blade.php
@@ -15,6 +15,23 @@
     <meta name="msapplication-TileColor" content="#8000ff">
     <meta name="msapplication-config" content="/images/favicon/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
+    <!-- Primary Meta Tags -->
+    <meta name="title" content="Keine Kinder zweiter Klasse!">
+    <meta name="description" content="Minderjährige Geflüchtete haben ein Recht auf kindgerechte Betreuung. Missstände wie im Jugendasylzentrum Lilienberg lassen sich verhindern: Schaffen Sie eine tragfähige Basis für die Betreuung geflüchteter Jugendlicher und deren Begleitung in die Selbständigkeit.">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ url() }}">
+    <meta property="og:title" content="Keine Kinder zweiter Klasse!">
+    <meta property="og:description" content="Minderjährige Geflüchtete haben ein Recht auf kindgerechte Betreuung. Missstände wie im Jugendasylzentrum Lilienberg lassen sich verhindern: Schaffen Sie eine tragfähige Basis für die Betreuung geflüchteter Jugendlicher und deren Begleitung in die Selbständigkeit.">
+    <meta property="og:image" content="{{ url("images/og.jpg ")}}">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="{{ url() }}">
+    <meta property="twitter:title" content="Keine Kinder zweiter Klasse!">
+    <meta property="twitter:description" content="Minderjährige Geflüchtete haben ein Recht auf kindgerechte Betreuung. Missstände wie im Jugendasylzentrum Lilienberg lassen sich verhindern: Schaffen Sie eine tragfähige Basis für die Betreuung geflüchteter Jugendlicher und deren Begleitung in die Selbständigkeit.">
+    <meta property="twitter:image" content="{{ url("images/og.jpg ")}}">
 </head>
 <body>
 
