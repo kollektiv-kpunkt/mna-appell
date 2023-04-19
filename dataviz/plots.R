ca03e16e65a8401006401df2200bab2808c6873a
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-04-19 20:47:32 +0200

    Update working directory path

--- dataviz/plots.R
+++ dataviz/plots.R
@@ -3,7 +3,7 @@ library(foreign)
 library(lubridate)
 
 ## Read from file ./date-signatures-2023-04-19.csv to df_dates
-df_dates <- read_csv("storage/app/supporters/date-signatures.csv")
+df_dates <- read_csv("../storage/app/supporters/date-signatures.csv")
 
 ## Add cummulative number of signatures up to that date to df_dates
 for (i in 1:nrow(df_dates)) {
@@ -23,6 +23,6 @@ plot <- ggplot(df_dates, aes(x = date, y = signatures)) +
     scale_fill_identity(name = "Test", guide = "legend", labels = c("m1"))
 
 ## Save the plot to a file named according to the current date
-filename <- paste0("dataviz/plot-", format(Sys.Date(), "%Y-%m-%d"), "_", format(Sys.time(), "%H-%M-%S"), ".png")
+filename <- paste0("public/plots/plot-", format(Sys.Date(), "%Y-%m-%d"), "_", format(Sys.time(), "%H-%M-%S"), ".png")
 ggsave(plot, filename = filename, width = 10, height = 7.5, dpi = 300)
-ggsave(plot, filename = "dataviz/plot-latest.png", width = 10, height = 7.5, dpi = 300)
+ggsave(plot, filename = "public/plots/plot-latest.png", width = 10, height = 7.5, dpi = 300)
