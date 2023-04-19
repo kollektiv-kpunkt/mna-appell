01785066df1a283c7a76f177aa696d8f5b41a808
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-04-19 21:02:53 +0200

    Add wd commandArg

--- dataviz/plots.R
+++ dataviz/plots.R
@@ -1,9 +1,17 @@
+library(commandArgs)
 library(tidyverse)
 library(foreign)
 library(lubridate)
 
+## Read the command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) > 0) {
+    wd <- args[1]
+    setwd(wd)
+}
+
 ## Read from file ./date-signatures-2023-04-19.csv to df_dates
-df_dates <- read_csv("../storage/app/supporters/date-signatures.csv")
+df_dates <- read_csv("storage/app/supporters/date-signatures.csv")
 
 ## Add cummulative number of signatures up to that date to df_dates
 for (i in 1:nrow(df_dates)) {
@@ -23,6 +31,6 @@ plot <- ggplot(df_dates, aes(x = date, y = signatures)) +
     scale_fill_identity(name = "Test", guide = "legend", labels = c("m1"))
 
 ## Save the plot to a file named according to the current date
-filename <- paste0("../public/plots/plot-", format(Sys.Date(), "%Y-%m-%d"), "_", format(Sys.time(), "%H-%M-%S"), ".png")
+filename <- paste0("public/plots/plot-", format(Sys.Date(), "%Y-%m-%d"), "_", format(Sys.time(), "%H-%M-%S"), ".png")
 ggsave(plot, filename = filename, width = 10, height = 7.5, dpi = 300)
-ggsave(plot, filename = "../public/plots/plot-latest.png", width = 10, height = 7.5, dpi = 300)
+ggsave(plot, filename = "public/plots/plot-latest.png", width = 10, height = 7.5, dpi = 300)
